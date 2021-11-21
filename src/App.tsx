e90495dc93d42ab12c2237140de28e5a30f680b8
temppone <gtvtempone@gmail.com>
2021-11-21 17:30:11 -0300

    chore: rename components folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Provider } from "react-redux";
-import Notification from "./compoenents/Notification";
+import Notification from "./components/Notification";
 import store from "./store";
 
 function App() {
